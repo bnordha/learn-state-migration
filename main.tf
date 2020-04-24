af99d2c9f743ba3541a4e79488013f16eebd7530
bernd.nordhausen-at-649524209221 <bernd.nordhausen@accenture.com>
2020-04-24 17:17:40 +0800

    .

---
+++ main.tf
@@ -0,0 +1,25 @@
+# Terraform configuration
+terraform {
+  backend "remote" {
+    hostname      = "app.terraform.io"
+    organization  = "NordhausenIO"
+
+    workspaces {
+      name = "state-migration"
+    }
+  }
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
